0eaf49cb6811a226925a840e26286417a38c5631
jomo0825 <jomo0825@gmail.com>
2020-11-26 20:47:26 +0800

    Initial commit

---
+++ Assets/Scripts/MyGameManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.SceneManagement;
+
+public class MyGameManager : MonoBehaviour
+{
+    public bool VRMode = false;
+
+    void Start()
+    {
+        if (VRMode)
+        {
+            StartCoroutine(StartVR());
+        }
+        else
+        {
+            StartCoroutine(StopVR());
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ToggleVR()
+    {
+        if (XRSettings.loadedDeviceName != "cardboard")
+        {
+            StartCoroutine(StartVR());
+        }
+        else
+        {
+            StartCoroutine(StopVR());
+        }
+    }
+
+    IEnumerator StopVR()
+    {
+        XRSettings.LoadDeviceByName("none");
+        yield return null;
+        XRSettings.enabled = false;
+    }
+
+    IEnumerator StartVR()
+    {
+        XRSettings.LoadDeviceByName("cardboard");
+        yield return null;
+        XRSettings.enabled = true;
+    }
+
+    public void GoToScene(string name)
+    {
+        StartCoroutine(GoToSceneWithDelay(1.0f, name));
+    }
+
+    IEnumerator GoToSceneWithDelay(float delay, string name)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(name);
+    }
+}
